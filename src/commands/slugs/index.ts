efacc9a301c6d5df7a2a4c0e0439b390b0295f16
Eric Black <eblack@salesforce.com>
2025-02-13 11:01:50 -0800

    Migrate to oclif

---
+++ src/commands/slugs/index.ts
@@ -0,0 +1,27 @@
+import {Command, flags} from '@heroku-cli/command'
+import * as Heroku from '@heroku-cli/schema'
+import {ux} from '@oclif/core'
+
+export default class SlugsIndex extends Command {
+  static description = 'list recent slugs on application'
+
+  static examples = [
+    '$ heroku slugs --app myapp',
+  ]
+
+  static flags = {
+    app: flags.app({required: true}),
+    remote: flags.remote(),
+  }
+
+  async run() {
+    const {flags} = await this.parse(SlugsIndex)
+    const {app} = flags
+    const {body: releases} = await this.heroku.get<Heroku.Release[]>(`/apps/${app}/releases`, {
+      headers: {
+        Range: 'version ..; order=desc',
+      },
+    })
+    for (const r of releases.filter(r => r.slug)) ux.log(`v${r.version}: ${r.slug?.id}`)
+  }
+}
