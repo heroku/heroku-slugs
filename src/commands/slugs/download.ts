ffb848160d62cffa58a9950a7f7d8ca28edaa82f
Eric <eblack@salesforce.com>
2025-02-26 15:44:55 -0800

    feat: add logging, no-extract-slug option, and issues download notification. also adds test (#29)
    * Add a test for download command and some additional logging output
    
    * Add option to skip slug extraction
    
    * Remove tarball
    
    * Fix tests
    
    * Fix linting
    
    * Improve download description
    
    * Add notification for downloads that take longer than 10 seconds

--- src/commands/slugs/download.ts
+++ src/commands/slugs/download.ts
@@ -1,4 +1,5 @@
 import {Command, flags} from '@heroku-cli/command'
+import {Notification, notify} from '@heroku-cli/notifications'
 import * as Heroku from '@heroku-cli/schema'
 import {Args, ux} from '@oclif/core'
 import {execSync} from 'node:child_process'
@@ -10,14 +11,16 @@ export default class SlugsDownload extends Command {
     slug: Args.string({description: 'name or ID of slug'}),
   }
 
-  static description = 'download a slug to <APP_NAME>/slug.tar.gz and extract it'
+  static description = "download a slug's tarball to <APP_NAME>/slug.tar.gz and then extract the slug"
 
   static examples = [
     '$ heroku slugs:download --app example-app v2',
+    '$ heroku slugs:download --app example-app v2 --no-extract-slug',
   ]
 
   static flags = {
     app: flags.app({required: true}),
+    'no-extract-slug': flags.boolean({char: 'e', default: false, description: 'skip extracting slug after download'}),
     remote: flags.remote(),
   }
 
@@ -25,6 +28,7 @@ export default class SlugsDownload extends Command {
     const {args, flags} = await this.parse(SlugsDownload)
     const {app} = flags
     const {slug} = args
+    const noExtractSlug = flags['no-extract-slug']
 
     let id = slug
     if (!id) {
@@ -47,8 +51,25 @@ export default class SlugsDownload extends Command {
       this.error('This slug has no blob to download.')
     }
 
+    ux.log(`Downloading slug ${id} to ${app}/slug.tar.gz`)
+    const downloadStarted = Date.now()
     execSync(`mkdir ${app}`)
     await download(appSlug.blob.url, `${app}/slug.tar.gz`, {progress: true})
-    execSync(`tar -xf ${app}/slug.tar.gz -C ${app}`)
+
+    if (!noExtractSlug) {
+      ux.action.start(`Extracting ${app}/slug.tar.gz`)
+      execSync(`tar -xf ${app}/slug.tar.gz -C ${app}`)
+      ux.action.stop()
+    }
+
+    const downloadTime = Date.now() - downloadStarted
+    if (downloadTime > 10 * 1000) {
+      const notification: { subtitle?: string } & Notification = {
+        message: 'download is finished',
+        subtitle: 'heroku slugs:download',
+        title: app,
+      }
+      notify(notification)
+    }
   }
 }
---
+++ test/commands/slugs/download.test.ts
@@ -0,0 +1,52 @@
+import {expect} from 'chai'
+import * as fs from 'fs-extra'
+import * as nock from 'nock'
+import * as childProcess from 'node:child_process'
+import {stderr, stdout} from 'stdout-stderr'
+
+import Cmd from '../../../src/commands/slugs/download'
+import runCommand from '../../helpers/run-command'
+
+import sinon = require('sinon')
+
+describe('slugs:download', function () {
+  let extractTarStub: sinon.SinonStub
+
+  beforeEach(function () {
+    nock.cleanAll()
+    extractTarStub = sinon.stub(childProcess, 'execSync')
+  })
+
+  afterEach(async function () {
+    extractTarStub.restore()
+    fs.remove('myapp')
+  })
+
+  it('downloads a slug', async function () {
+    // Mock the Heroku API response
+    nock('https://api.heroku.com')
+      .get('/apps/myapp/slugs/slug-id')
+      .reply(200, {
+        blob: {
+          url: 'https://slug-url.com/slug.tgz',
+        },
+      })
+
+    // Mock the slug download URL
+    const slugContent = 'mock-slug-content'
+    nock('https://slug-url.com')
+      .get('/slug.tgz')
+      .reply(200, slugContent)
+
+    await runCommand(Cmd, [
+      '--app',
+      'myapp',
+      'slug-id',
+    ])
+
+    expect(extractTarStub.withArgs('mkdir myapp').calledOnce).to.eq(true)
+    expect(extractTarStub.withArgs('tar -xf myapp/slug.tar.gz -C myapp').calledOnce).to.eq(true)
+    expect(stdout.output).to.include('Downloading slug slug-id to myapp/slug.tar.gz')
+    expect(stderr.output).to.include('Extracting myapp/slug.tar.gz... done')
+  })
+})
